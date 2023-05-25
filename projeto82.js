422bcbda97f2c5299cb3b209c61fb48c3e42dde8
yasmin0715 <129914162+yasmin0715@users.noreply.github.com>
2023-05-24 22:07:45 -0300

    Add files via upload

---
+++ projeto82.js
@@ -0,0 +1,27 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Rebeca";
+            var lastName = "Kapoor";
+
+            
+            var fullName =lastName;
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+
+firstNamecancat(lastName);
+
+            var add =a + b;
+            console.log(add);
+
+
+            var sub = a - b;
+            console.log(sub);
+
+
+            var multiple = a * b;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
